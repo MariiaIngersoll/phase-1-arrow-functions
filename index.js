4ddcca2108280c44d75b44de10b9156862a38fee
+ <+>
2023-02-23 23:52:34 -0500

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function() {
+    let a = 2000;
+    let b = 100;
+    return a/b;
+}
+divide(2000, 100);
+
+const square = parameter1 => parameter1 * parameter1;
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+}
+add()
